7ddbd50745d67bacb947c6d99f9d506dddb64aac
rachit rawat <rachitthakur311@gmail.com>
2020-07-12 19:55:22 +0530

    Updates

---
+++ precache-manifest.e5a4a20ebe157ba551e938ea384a7311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8dc5cbca560191c2d2f89383c3be56",
+    "url": "/deploy-reactapp/index.html"
+  },
+  {
+    "revision": "dc176ea3ebfd2c2adb1f",
+    "url": "/deploy-reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/deploy-reactapp/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-reactapp/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc176ea3ebfd2c2adb1f",
+    "url": "/deploy-reactapp/static/js/main.65fbf1b9.chunk.js"
+  },
+  {
+    "revision": "0e961cf602c7e6a5e719",
+    "url": "/deploy-reactapp/static/js/runtime-main.d926a3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
